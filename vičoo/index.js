c898921399fe898f9fb64bfd6e7995928fc687fa
mariox03 <66534039+mariox03@users.noreply.github.com>
2021-12-22 10:55:00 +0100

    Add files via upload

---
+++ vičoo/index.js
@@ -0,0 +1,28 @@
+
+// Accordion 
+function myAccFunc() {
+    var x = document.getElementById("font");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+
+document.getElementById("myBtn").click();
+
+
+function w3_open() {
+    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("myOverlay").style.display = "block";
+}
+ 
+function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("myOverlay").style.display = "none";
+}
+
+function poskusi()
+{
+	window.alert("Ta spletna stran še ni na voljo, poskusite kasneje.")
+}
